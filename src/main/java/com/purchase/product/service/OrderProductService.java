1be83f5efafba76eec225c13f8bcec41ef109972
easywaldo <easywaldoji@gmail.com>
2022-01-02 00:01:53 +0900

    OrderProductSerivce 내의 사용하지 않는 의존성 제거

--- src/main/java/com/purchase/product/service/OrderProductService.java
+++ src/main/java/com/purchase/product/service/OrderProductService.java
@@ -1,31 +1,19 @@
 package com.purchase.product.service;
 
-import com.purchase.order.repository.OrderInfoRepository;
 import com.purchase.order.response.PgResponse;
 import com.purchase.order.service.payment.PaymentFactory;
 import com.purchase.order.service.payment.RegisterOrderService;
 import com.purchase.product.command.OrderProductCommand;
-import com.purchase.product.repository.ProductRepository;
-import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderProductService {
-    private final ProductRepository productRepository;
-    private final PaymentFactory paymentFactory;
     private final RegisterOrderService registerOrderService;
-    private final OrderInfoRepository orderInfoRepository;
 
-    @Builder
-    public OrderProductService(ProductRepository productRepository,
-                               PaymentFactory paymentFactory,
-                               RegisterOrderService registerOrderService,
-                               OrderInfoRepository orderInfoRepository) {
-        this.productRepository = productRepository;
-        this.paymentFactory = paymentFactory;
+    @Autowired
+    public OrderProductService(RegisterOrderService registerOrderService) {
         this.registerOrderService = registerOrderService;
-        this.orderInfoRepository = orderInfoRepository;
     }
 
     public PgResponse processOrder(OrderProductCommand orderProductCommand) {
